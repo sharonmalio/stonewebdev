fe48f147f3f10b288e29a313e3633719a5cf20fd
smalio <kaninimalio@gmail.com>
2018-10-09 16:43:34 +0300

    working on the PWA

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/worker.js').then(function (registration) {
+        navigator.serviceWorker.register('./worker.js').then(function (registration) {
              console.log("Registration was successful");
         }, function (err) {
              console.log("registration failed :(", err);
--- public/worker.js
+++ public/worker.js
@@ -1,4 +1,8 @@
+'use strict';
 var CACHE_NAME = 'stoneweb-cache';
+const version = '<?php echo $this->getVersion() ?>';
+const offlinePage = '<?php echo $this->getOfflinePageUrl() ?>';
+const urlBlacklist ='<?php echo json_encode($this->getOfflineUrlBlacklist()) ?>';
 var resources = [
   '/css/bootstrap.min.css',
   '/css/bootstrap.css',
@@ -6,29 +10,104 @@ var resources = [
   '/manifest.json',
 ];
 
-// Open + Cache + Verify cache
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        // console.log('Opened cache');
-        return cache.addAll(resources);
-      })
-  );
+
+function updateStaticCache() {
+    return caches.open(version)
+        .then(cache => {
+            return cache.addAll([
+                offlinePage
+            ]);
+        });
+}
+
+function clearOldCaches() {
+    return caches.keys().then(keys => {
+        return Promise.all(
+            keys
+                .filter(key => key.indexOf(version) !== 0)
+                .map(key => caches.delete(key))
+        );
+    });
+}
+
+function isHtmlRequest(request) {
+    return request.headers.get('Accept').indexOf('text/html') !== -1;
+}
+
+
+function isBlacklisted(url) {
+    return urlBlacklist.filter(bl => url.indexOf(bl) == 0).length > 0;
+}
+
+
+function isCachableResponse(response) {
+    return response && response.ok;
+}
+
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        updateStaticCache()
+            .then(() => self.skipWaiting())
+    );
 });
 
-// Load cache if present otherwise install
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        }
-      )
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        clearOldCaches()
+            .then(() => self.clients.claim())
     );
-  });
+});
+
+
+self.addEventListener('fetch', event => {
+    let request = event.request;
+
+    if (request.method !== 'GET') {
+        
+        if (!navigator.onLine && isHtmlRequest(request)) {
+            return event.respondWith(caches.match(offlinePage));
+        }
+        return;
+    }
+
+    if (isHtmlRequest(request)) {
+        
+        event.respondWith(
+            fetch(request)
+                .then(response => {
+                    if (isCachableResponse(response) && !isBlacklisted(response.url)) {
+                        let copy = response.clone();
+                        caches.open(version).then(cache => cache.put(request, copy));
+                    }
+                    return response;
+                })
+                .catch(() => {
+                    return caches.match(request)
+                        .then(response => {
+                            if (!response && request.mode == 'navigate') {
+                                return caches.match(offlinePage);
+                            }
+                            return response;
+                        });
+                })
+        );
+    } else {
+if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin')
+      return
+        event.respondWith(
+            caches.match(request)
+                .then(response => {
+                    return response || fetch(request)
+                            .then(response => {
+                                if (isCachableResponse(response)) {
+                                    let copy = response.clone();
+                                    caches.open(version).then(cache => cache.put(request, copy));
+                                }
+                                return response;
+                            })
+                })
+        );
+    }
+});
