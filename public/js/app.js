173cad2166a849ceecbbb06f17fda24594a3d7dc
smalio <kaninimalio@gmail.com>
2018-10-09 17:33:45 +0300

    working on the PWA

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,13 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('./worker.js').then(function (registration) {
+        navigator.serviceWorker.register('/worker.js').then(function (registration) {
              console.log("Registration was successful");
         }, function (err) {
              console.log("registration failed :(", err);
         });
     });
 }
+//navigator.serviceWorker && navigator.serviceWorker.register('/worker.js').then(function(registration) {
+//	  console.log('Excellent, registered with scope: ', registration.scope);
+//	});
